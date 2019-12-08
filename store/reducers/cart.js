2edd843a718659bcbb5fedcadfbbd5d7aa9bd2c5
Weisberg Barros <weisberg.fontes@nlt.com.br>
2019-12-08 17:44:52 -0300

    Deleting Items

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,6 +1,7 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 import { ADD_ORDER } from "../actions/orders";
+import { DELETE_PRODUCT } from "../actions/products";
 
 const initialState = {
   items: {},
@@ -56,6 +57,18 @@ export default (state = initialState, action) => {
       };
     case ADD_ORDER:
       return initialState;
+    case DELETE_PRODUCT:
+      if (!state.items[action.pid]) {
+        return state;
+      }
+      const updateItems = { ...state.items };
+      const itemTotal = state.items[action.pid].sum;
+      delete updateItems[action.pid];
+      return {
+        ...state,
+        items: updateItems,
+        totalAmount: state.totalAmount - itemTotal
+      };
   }
 
   return state;
